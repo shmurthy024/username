af629719b894c37c8716fc8f7be779348cf9b5c1
shmurthy024 <shreemurthy04@gmail.com>
2020-11-13 11:01:58 -0800

    Updated the username.sh script and now it runs correctly

--- username.sh
+++ username.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 # username.sh
 # Shree Murthy
+echo "Enter a username that starts with a lowercase letter and can only have lowercase letters, digits and underscores. Also the username has to be between 3-12 characters!"
+echo " "
 echo "Enter a Username : "
 read -r NAME
-while echo "$NAME" | grep -E -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter a valid username - Must start with a lowercase letter, and can only have lowercase letters, underscores and digits! Also it can only be between 3-12 characters. Try again!"
+	echo "Enter a Username : "
 	read -r NAME
 done
 echo "Thank you"
